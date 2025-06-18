73beac4dd430f1146ed685439dbf4a6aa718abc5
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2025-06-18 18:02:04 +0300

    3 laba

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+    }
+}
